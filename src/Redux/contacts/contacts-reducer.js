a0f5db71689b9b473848a3d7dab71ce2e9281bef
Dmitry Mirchuk <79853644+dimon-mirchuk@users.noreply.github.com>
2021-12-02 09:29:19 +0300

    fix bug

--- src/Redux/contacts/contacts-reducer.js
+++ src/Redux/contacts/contacts-reducer.js
@@ -4,9 +4,9 @@ import { addContact, deleteContact, filterContact } from "./contacts-actions";
 
 const items = createReducer([], {
   [addContact]: (state, { payload }) =>
-    state.find(({ name }) => name !== payload.name)
-      ? [...state, payload]
-      : alert(`${payload.name} is already in contacts`),
+    state.find(({ name }) => name === payload.name)
+      ? alert(`${payload.name} is already in contacts`)
+      : [...state, payload],
   [deleteContact]: (state, { payload }) =>
     state.filter(({ id }) => id !== payload),
 });
